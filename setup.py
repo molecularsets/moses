ba5dff955e4dc03f45b3b43f85ca126d2d4b3281
“rauf-kurbanov” <kurbanov.re@gmail.com>
2018-05-17 12:59:04 +0300

    inital commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='mnist4molecules',
+      version='0.1.0',
+      python_requires='>=3.5.0',
+      # Make sure to pin versions of install_requires
+      install_requires=[],
+      # TODO
+      # description='??',
+      author='Neuromation Team',
+      author_email='engineering@neuromation.io',
+      # TODO (rauf): make repo public and update URL
+      # TODO license='??'
+      )
