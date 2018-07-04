26397a3fed42cd42b996a9ce04f373f9fb0e8ac2
Sergey Golovanov <sergey_xg@mail.ru>
2018-07-04 23:52:26 +0300

    update default parameters for aae

--- mnist4molecules/aae/config.py
+++ mnist4molecules/aae/config.py
@@ -5,9 +5,9 @@ def get_parser():
     parser = argparse.ArgumentParser()
 
     model_arg = parser.add_argument_group('Model')
-    model_arg.add_argument('--embedding_size', type=int, default=16,
+    model_arg.add_argument('--embedding_size', type=int, default=32,
                            help='Embedding size in encoder and decoder')
-    model_arg.add_argument('--encoder_hidden_size', type=int, default=128,
+    model_arg.add_argument('--encoder_hidden_size', type=int, default=1024,
                            help='Size of hidden state for lstm layers in encoder')
     model_arg.add_argument('--encoder_num_layers', type=int, default=1,
                            help='Number of lstm layers in encoder')
@@ -15,26 +15,26 @@ def get_parser():
                            help='If true to use bidirectional lstm layers in encoder')
     model_arg.add_argument('--encoder_dropout', type=float, default=0,
                            help='Dropout probability for lstm layers in encoder')
-    model_arg.add_argument('--decoder_hidden_size', type=int, default=128,
+    model_arg.add_argument('--decoder_hidden_size', type=int, default=1024,
                            help='Size of hidden state for lstm layers in decoder')
     model_arg.add_argument('--decoder_num_layers', type=int, default=1,
                            help='Number of lstm layers in decoder')
     model_arg.add_argument('--decoder_dropout', type=float, default=0,
                            help='Dropout probability for lstm layers in decoder')
-    model_arg.add_argument('--latent_size', type=int, default=8,
+    model_arg.add_argument('--latent_size', type=int, default=128,
                            help='Size of latent vectors')
-    model_arg.add_argument('--discriminator_layers', nargs='+', type=int, default=[128, 64],
+    model_arg.add_argument('--discriminator_layers', nargs='+', type=int, default=[256, 128],
                            help='Numbers of features for linear layers in discriminator')
 
 
     train_arg = parser.add_argument_group('Training')
     train_arg.add_argument('--pretrain_epochs', type=int, default=0,
                            help='Number of epochs for autoencoder pretraining')
-    train_arg.add_argument('--train_epochs', type=int, default=10,
+    train_arg.add_argument('--train_epochs', type=int, default=50,
                            help='Number of epochs for autoencoder training')
-    train_arg.add_argument('--n_batch', type=int, default=64,
+    train_arg.add_argument('--n_batch', type=int, default=128,
                            help='Size of batch')
-    train_arg.add_argument('--lr', type=float, default=1e-2,
+    train_arg.add_argument('--lr', type=float, default=1e-3,
                            help='Learning rate')
 
     return parser
