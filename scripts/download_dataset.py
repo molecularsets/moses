f4a74d5d4b71e71bfa58e1ea2706480f80808788
Sergey Golovanov <sergey_xg@mail.ru>
2018-10-11 14:16:23 +0300

    fix script for dataset

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -9,13 +9,13 @@ def get_parser():
 
     parser.add_argument('--output_dir', type=str, default='./data',
                            help='Directory for downloaded dataset')
-    parser.add_argument('--dataset_url', type=str, default='https://media.githubusercontent.com/media/neuromation/mnist4molecules/master-fixes/data/dataset.csv?token=AORf9_1XuBXJjCmYV--t6f1Ui5aVe-WEks5bvLClwA%3D%3D',
+    parser.add_argument('--dataset_url', type=str, default='',
                            help='URL of dataset')
     parser.add_argument('--no_subset', action='store_true',
                            help='Do not create subsets for training and testing')
-    parser.add_argument('--train_size', type=int, default=200000,
+    parser.add_argument('--train_size', type=int, default=None,
                            help='Size of training dataset')
-    parser.add_argument('--test_size', type=int, default=10000,
+    parser.add_argument('--test_size', type=int, default=None,
                            help='Size of testing dataset')
     parser.add_argument('--seed', type=int, default=0,
                            help='Random state')
@@ -39,9 +39,12 @@ def main(config):
     test_data = data[data['SPLIT'] == 'test']
     test_scaffolds_data = data[data['SPLIT'] == 'test_scaffolds']
 
-    train_data = train_data.sample(config.train_size, random_state=config.seed)
-    test_data = test_data.sample(config.test_size, random_state=config.seed)
-    test_scaffolds_data = test_scaffolds_data.sample(config.test_size, random_state=config.seed)
+    if config.train_size is not None:
+        train_data = train_data.sample(config.train_size, random_state=config.seed)
+
+    if config.test_size is not None:
+        test_data = test_data.sample(config.test_size, random_state=config.seed)
+        test_scaffolds_data = test_scaffolds_data.sample(config.test_size, random_state=config.seed)
 
     train_data.to_csv(os.path.join(config.output_dir, 'train.csv'), index=False)
     test_data.to_csv(os.path.join(config.output_dir, 'test.csv'), index=False)
