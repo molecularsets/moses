60cd519a0de8ac5d84f315fe9f2997c75725a99b
hbq1 <y.kemaev@gmail.com>
2019-01-17 11:31:06 +0000

    fix argument type

--- moses/script_utils.py
+++ moses/script_utils.py
@@ -50,7 +50,7 @@ def add_train_args(parser):
                             type=str, default='model.pt',
                             help='Where to save the model')
     common_arg.add_argument('--save_frequency',
-                            type=str, default=20,
+                            type=int, default=20,
                             help='How often to save the model')
     common_arg.add_argument('--log_file',
                             type=str, default='log.txt',
