a4583e4d6a28f01d55c4695c58436d69f3737d2c
hbq1 <y.kemaev@gmail.com>
2019-01-17 16:03:35 +0000

    AAE refactoring

--- scripts/aae/train.py
+++ scripts/aae/train.py
@@ -2,7 +2,6 @@ import torch
 
 from moses.aae import AAE, AAETrainer, get_parser as aae_parser
 from moses.script_utils import add_train_args, read_smiles_csv, set_seed
-from moses.utils import CharVocab
 
 
 def get_parser():
@@ -11,24 +10,20 @@ def get_parser():
 
 def main(config):
     set_seed(config.seed)
-
-    train = read_smiles_csv(config.train_load)
-
-    vocab = CharVocab.from_data(train)
-    torch.save(config, config.config_save)
-    torch.save(vocab, config.vocab_save)
-
     device = torch.device(config.device)
 
-    model = AAE(vocab, config)
-    model = model.to(device)
-
+    train_data = read_smiles_csv(config.train_load)
     trainer = AAETrainer(config)
-    trainer.fit(model, train)
+
+    vocab = trainer.get_vocabulary(train_data)
+    model = AAE(vocab, config).to(device)
+
+    trainer.fit(model, train_data)
 
     model.to('cpu')
     torch.save(model.state_dict(), config.model_save)
-
+    torch.save(config, config.config_save)
+    torch.save(vocab, config.vocab_save)
 
 if __name__ == '__main__':
     parser = get_parser()
