def2b18d5cb637a1c5d909b471e3c241e669b7fb
hbq1 <y.kemaev@gmail.com>
2019-01-17 16:08:36 +0000

    char-rnn refactoring

--- moses/char_rnn/config.py
+++ moses/char_rnn/config.py
@@ -6,15 +6,24 @@ def get_parser():
 
     # Model
     model_arg = parser.add_argument_group('Model')
+    model_arg.add_argument("--num_layers", type=int, default=3,
+                           help="Number of LSTM layers")
+    model_arg.add_argument("--hidden", type=int, default=600,
+                           help="Hidden size")
+    model_arg.add_argument("--dropout", type=float, default=0.2,
+                           help="dropout between LSTM layers except for last")
 
-    model_arg.add_argument("--val_load", type=str, help="Input data in csv format to validation")
-    model_arg.add_argument("--num_layers", type=int, default=3, help="Number of LSTM layers")
-    model_arg.add_argument("--hidden", type=int, default=600, help="Hidden size")
-    model_arg.add_argument("--dropout", type=float, default=0.2, help="dropout between LSTM layers except for last")
-    model_arg.add_argument("--batch", type=int, default=64, help="Batch size")
-    model_arg.add_argument("--num_epochs", type=int, default=50, help="Number of epochs")
-    model_arg.add_argument('--lr', type=float, default=0.001, help='Initial lr value')
-    model_arg.add_argument('--n_jobs', type=int, default=1, help='Number of threads')
+    # Train
+    train_arg = parser.add_argument_group('Training')
+    train_arg.add_argument('--train_epochs', type=int, default=50,
+                           help='Number of epochs for model training')
+    train_arg.add_argument('--n_batch', type=int, default=128,
+                           help='Size of batch')
+    train_arg.add_argument('--lr', type=float, default=1e-3,
+                           help='Learning rate')
+    train_arg.add_argument('--n_jobs', type=int, default=1,
+                           help='Number of threads')
+
     return parser
 
 
--- scripts/char_rnn/train.py
+++ scripts/char_rnn/train.py
@@ -1,7 +1,6 @@
 import torch
 
 from moses.char_rnn.config import get_parser as char_rnn_parser
-from moses.char_rnn.datautils import OneHotCorpus
 from moses.char_rnn.model import CharRNN
 from moses.char_rnn.trainer import CharRNNTrainer
 from moses.script_utils import add_train_args, read_smiles_csv, set_seed
@@ -13,23 +12,20 @@ def get_parser():
 
 def main(config):
     set_seed(config.seed)
-
-    train = read_smiles_csv(config.train_load)
-
     device = torch.device(config.device)
 
-    corpus = OneHotCorpus(config.batch, device)
-
-    train_dataloader = corpus.fit(train).transform(train)
-
-    model = CharRNN(corpus.vocab, config.hidden, config.num_layers, config.dropout, device).to(device)
+    train_data = read_smiles_csv(config.train_load)
     trainer = CharRNNTrainer(config)
-    trainer.fit(model, train_dataloader)
 
+    vocab = trainer.get_vocabulary(train_data)
+    model = CharRNN(vocab, config).to(device)
+
+    trainer.fit(model, train_data)
+
+    model = model.to('cpu')
     torch.save(model.state_dict(), config.model_save)
     torch.save(config, config.config_save)
-    torch.save(corpus.vocab, config.vocab_save)
-
+    torch.save(vocab, config.vocab_save)
 
 if __name__ == '__main__':
     parser = get_parser()
