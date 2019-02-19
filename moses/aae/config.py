adb010ecbef202e5c527e7c067d55a054afdfa4e
Daniil Polykovskiy <daniil.polykovskiy@gmail.com>
2019-02-19 16:19:08 +0000

    pep8 for AAE

--- moses/aae/config.py
+++ moses/aae/config.py
@@ -9,23 +9,30 @@ def get_parser(parser=None):
     model_arg.add_argument('--embedding_size', type=int, default=32,
                            help='Embedding size in encoder and decoder')
     model_arg.add_argument('--encoder_hidden_size', type=int, default=512,
-                           help='Size of hidden state for lstm layers in encoder')
+                           help='Size of hidden state for '
+                                'lstm layers in encoder')
     model_arg.add_argument('--encoder_num_layers', type=int, default=1,
                            help='Number of lstm layers in encoder')
     model_arg.add_argument('--encoder_bidirectional', type=bool, default=True,
-                           help='If true to use bidirectional lstm layers in encoder')
+                           help='If true to use bidirectional lstm '
+                                'layers in encoder')
     model_arg.add_argument('--encoder_dropout', type=float, default=0,
-                           help='Dropout probability for lstm layers in encoder')
+                           help='Dropout probability for lstm '
+                                'layers in encoder')
     model_arg.add_argument('--decoder_hidden_size', type=int, default=512,
-                           help='Size of hidden state for lstm layers in decoder')
+                           help='Size of hidden state for lstm '
+                                'layers in decoder')
     model_arg.add_argument('--decoder_num_layers', type=int, default=2,
                            help='Number of lstm layers in decoder')
     model_arg.add_argument('--decoder_dropout', type=float, default=0,
-                           help='Dropout probability for lstm layers in decoder')
+                           help='Dropout probability for lstm '
+                                'layers in decoder')
     model_arg.add_argument('--latent_size', type=int, default=16,
                            help='Size of latent vectors')
-    model_arg.add_argument('--discriminator_layers', nargs='+', type=int, default=[640, 256],
-                           help='Numbers of features for linear layers in discriminator')
+    model_arg.add_argument('--discriminator_layers', nargs='+', type=int,
+                           default=[640, 256],
+                           help='Numbers of features for linear '
+                                'layers in discriminator')
 
     train_arg = parser.add_argument_group('Training')
     train_arg.add_argument('--pretrain_epochs', type=int, default=0,
